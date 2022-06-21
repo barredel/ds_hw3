f4bfab07ced488ca962fd9e62e537c8761c6bfa6
barredel <bar.redel@campus.technion.ac.il>
2022-06-21 18:47:51 +0300

    1

--- data.py
+++ data.py
@@ -18,3 +18,46 @@ def get_folds():
     :return: sklearn KFold object that defines a specific partition to folds
     """
     return KFold(n_splits=5, shuffle=True, random_state=34)
+
+
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+
+def adjust_labels(y):
+    y_adjusted = list(map(lambda x: 0 if x <= 1 else 1, y))
+    return y_adjusted
+
+
+class StandardScaler:
+    pass
+
+    def __init__(self):
+
+
+
+    def fit(self, X):
+    “”” fit scaler by learning mean and standard deviation per feature “””
+
+
+    def transform(self, X):
+   ## “”” transform X by learned mean and standard deviation, and return it “””
+    transformed_data = np.array([np.array(list(df[features[0]])), np.array(list(df[features[0]]))])
+    print(transformed_data)
+    sum0 = transformed_data[0].sum()
+    min0 = transformed_data[0].min()
+    sum1 = transformed_data[1].sum()
+    min1 = transformed_data[1].min()
+    (transformed_data[0] - min0)/sum0
+    (transformed_data[1] - min1) / sum1
+    print(transformed_data)
+
+
+    def fit_transform(self, X):
+    “”” fit scaler by learning mean and standard deviation per feature, and then transform X “””
+
+
+
+
+
